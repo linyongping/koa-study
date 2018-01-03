77679611846be06af563ddd641c3acc8b506f831
linyongping <linyonging@outlook.com>
2018-01-03 23:37:38 +0800

    use koa router

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
 const Koa = require('koa');
 const koaBodyParser = require('koa-body-parser');
 const fs = require('fs')
-const router = require('koa-router')
+const Router = require('koa-router')
 
 const app = new Koa()
-const koaRouter = new router() 
+const router = new Router({ prefix: '/api' })
 
-koaRouter.get('/', async (ctx, next) => {
-    ctx.body = render('index.html')
+router.get('/', async(ctx, next) => {
+    ctx.body = await render('index.html')
+}).get('/todo', async(ctx, next) => {
+    ctx.body = await render('todo.html')
 })
 
-app.use( async ctx => {
-    const url = ctx.request.url;
-    let html = await route(url)
-    ctx.body = html
-})
+app.use(router.routes())
+    .use(router.allowedMethods())
 
 app.listen(3000, () => {
-    console.log('koa is runinng')
+    console.log('koa is runinng on port 3000')
 })
 
 const render = page => {
     let pageUrl = `./page/${page}`
     return new Promise((resolve, reject) => {
         fs.readFile(pageUrl, 'binary', (err, data) => {
-            if(err) {
+            if (err) {
                 reject(err)
-            }else {
+            } else {
                 resolve(data)
             }
         })
     })
-}
-
-const route = async url => {
-    let page = '404.html'
-    switch(url) {
-        case '/': page = 'index.html'; break;
-        case '/index': page = 'index.html'; break;
-        case '/todo': page = 'todo.html'; break;
-        case '/404': page = '404.html'; break; 
-    }
-    let html = await render(page)
-    return html
 }
