76ff3415a92ac71d84d4a2e86e8a0643746913dc
linyonging <linyongping@outlook.com>
2018-02-03 11:26:35 +0800

    node js sutyd

---
+++ src/copy.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const bin = new Buffer([0x68, 0x65, 0x6c, 0x6c, 0x6f]);
+
+const pathname = '../package.json';
+let dupBin = new Buffer(bin.length);
+bin.copy(dupBin);
+const factorial = n => {
+  console.log('n', n);
+  if (n === 1) {
+    return n;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+const result = factorial(9);
+
+console.log('bin', result);
